4c8042e4f6b6045494af05d9c6c2d64905a9dbcf
yaniar prayogo <yaniar prayogo>
2017-04-07 17:22:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/z0r0/mytravelguide/TravelMain.java
@@ -0,0 +1,13 @@
+package com.example.z0r0.mytravelguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TravelMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_travel_main);
+    }
+}
